434ac017aa5b17e294fa1ee5929d786c62557507
Dmytro Herman <dmytroherman@tutamail.com>
2026-03-23 10:23:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,39 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    """
+        A decorator that caches the results of function calls
+        Function called with the same arguments again returns
+        the cached value instead of execution
+    """
+
+    # Empty dictionary of {(args, kwargs): result}
+    arguments_map = {}
+
+    @wraps(func)
+    def inner(*args, **kwargs) -> Any:
+
+        # Convert types to make it hashable
+        sorted_kwargs = tuple(sorted(kwargs.items()))
+
+        # Create a unique fingerprint
+        cache_key = (args, sorted_kwargs)
+
+        # Check if we have calculated value
+        if cache_key in arguments_map:
+            # print(f"Cache found: {args} {kwargs}")
+            print("Getting from cache")
+            return arguments_map[cache_key]
+
+        # Execute the original function
+        # print(f"Running with arguments: {args} {kwargs}")
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+
+        # Save result to the map
+        arguments_map[cache_key] = result
+
+        return result
+    return inner

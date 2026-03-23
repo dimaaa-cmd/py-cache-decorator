31900934b547b12ba1c392c4654628c33296dda0
Dmytro Herman <dmytroherman@tutamail.com>
2026-03-23 17:30:09 +0200

    Removed temporary inspection comments

--- app/main.py
+++ app/main.py
@@ -15,24 +15,17 @@ def cache(func: Callable) -> Callable:
     @wraps(func)
     def inner(*args, **kwargs) -> Any:
 
-        # Convert types to make it hashable
         sorted_kwargs = tuple(sorted(kwargs.items()))
 
-        # Create a unique fingerprint
         cache_key = (args, sorted_kwargs)
 
-        # Check if we have calculated value
         if cache_key in arguments_map:
-            # print(f"Cache found: {args} {kwargs}")
             print("Getting from cache")
             return arguments_map[cache_key]
 
-        # Execute the original function
-        # print(f"Running with arguments: {args} {kwargs}")
         print("Calculating new result")
         result = func(*args, **kwargs)
 
-        # Save result to the map
         arguments_map[cache_key] = result
 
         return result
